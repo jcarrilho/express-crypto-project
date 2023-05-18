28f377d61d9decb411c33d6925755fe139679edc
Lucas Telles <lltellesss@gmail.com>
2023-05-18 06:41:27 -0300

    ..

--- config/session.config.js
+++ config/session.config.js
@@ -24,7 +24,7 @@ module.exports = app => {
                 maxAge: 6000000, // 60 * 1000ms  === 1 min
             },
             store: MongoStore.create({
-                mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/express-crypto-project"
+                mongoUrl: process.env.MONGODB_URI
             })
         })
     )
